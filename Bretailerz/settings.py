4cb67e2bbafbada4b0b11655b3e9e06a43758586
simonpojok <simonpojok@gmail.com>
2022-12-02 18:17:33 +0300

    done setting up the project

--- Bretailerz/settings.py
+++ Bretailerz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-btx0c6kgpj%%(dv(mgyw^+8b6wfj*ao)zr4oaqus4p+*7*ve8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Bretailerz.pythonanywhere.com']
+ALLOWED_HOSTS = ['Bretailerz.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
